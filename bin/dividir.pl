16d95b48cf9d766bcd2fab35e5bf4b10dec3c563
VovoLurdes <flaviog.info@gmail.com>
2015-02-24 17:40:34 -0300

    Dividir um arquivo

--- bin/dividir.pl
+++ bin/dividir.pl
@@ -13,22 +13,22 @@ use Flag;
 my $arquivo = "bolinha.gz"; 
 Flag::download("http://172.17.2.43/vd/xmls/foobar",$arquivo);
 #Flag::ler_sem_descompactar("tmp/$arquivo");
-#Flag::descompactar_de_verdade("tmp/$arquivo","$arquivo");
+Flag::descompactar_de_verdade("/tmp/$arquivo","testa");
 
-#$bla = '/Users/vovolurdes/Developer/emolga/bin/$arquivo';
+my $bla = '/Users/vovolurdes/Developer/emolga/bin/testa';
 
-#open my $fh, '<', $bla or croak "Não foi possível abrir o arquivo '$arquivo' $OS_ERROR";
+open my $fh, '<', $bla;
 
-#my $contador=0;
-#my $narquivo=1;
+my $contador=0;
+my $narquivo=1;
 
-#while (my $solo = <$fh>)
-#{ 
-#open my $fh_bla, '>', $arquivo_$narquivo;
-#print $arquivo_$narquivo $solo;
+while (my $solo = <$fh>)
+{ 
+open my $fh_bla, '>>', "louco$narquivo";
+print  $fh_bla $solo;
 
-#	if (m/<[\]produto>/xms){ $contador++;}
-# 	if ($contador==10){$n_arquivo++; close $fh_bla;}
-#}
+	if ($solo =~/<\/produto>/){ $contador++;}
+ 	if ($contador==10){$narquivo++; close $fh_bla; $contador=0;}
+}
   
 #close $fh;
